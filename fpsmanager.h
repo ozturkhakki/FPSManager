d597d5bb22763ce3f57175988eac302e2ee87107
Hakki Ozturk <hozturk0327@gmail.com>
2014-10-17 21:01:45 +0300

    Made class variables and functions static
    Made static so we can reach from anywhere within the program to the same instance of class.

--- fpsmanager.h
+++ fpsmanager.h
@@ -3,34 +3,46 @@
 
 #include <chrono>
 
+// It is better to have only one instance of this class
+// in the entire program. So, simply put, this is a static
+// class
+
 class FPSManager
 {
 public:
     FPSManager();
 
     //  Update the time
-    void updateTime();
+    static void updateTime();
+    
     //  Update the last time that is drawn frame
-    void updateLastFrameTime();
+    static void updateLastFrameTime();
+    
     //  Get elapsed time between last time a frame drawn and
     //  current time
-    float getElapsedTime();
+    static float getElapsedTime();
+    
+    //  Get current time 
+    static float getTime();
+    
     //  Makes calculation according to a second
     //
     //  Lets say you have an object that moves 5
     //  unit a second. If elapsed time is 100 miliseconds
     //  you will get 0.5(0.100 * 5) unit.
-    float makeCalcWithElapsedTime(float someValue);
+    static float makeCalcWithElapsedTime(float someValue);
+    
     //  Set frame. This way you will get true if a new frame
     //  should be drawn.
-    void setFPS(unsigned int fps);
+    static void setFPS(unsigned int fps);
+    
     //  Return true if a new frame should be drawn.
-    bool shouldDrawNewFrame();
+    static bool shouldDrawNewFrame();
 
 private:
-    float m_current;
-    float m_lastFrame;
-    unsigned int m_FPS{1000};
+    static float m_current;
+    static float m_lastFrame;
+    static unsigned int m_FPS{1000};
 
 };
 
