f29d5247c5236f46c005f29b4465dd07d5fd9f1a
Hakkı Ozturk <hozturk0327@gmail.com>
2015-01-05 01:03:59 +0200

    Added comment

--- example.cpp
+++ example.cpp
@@ -6,6 +6,7 @@ int main()
     unsigned int fpsCount = 0;
     FPSManager::Initialize(60);
 
+    // Run for 60 fps, which should be 1000 ms
     while(fpsCount <= 60){
         if(FPSManager::shouldDrawNewFrame()){
             std::cout << "Current fps drawn is    : " << fpsCount << std::endl;
