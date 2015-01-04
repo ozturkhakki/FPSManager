4e1ab2bdbffd0da672d8838e7214dadb4c939244
Hakki Ozturk <hozturk0327@gmail.com>
2015-01-04 02:15:48 +0200

    Fixed a problem, added private function getNow()

--- fpsmanager.cpp
+++ fpsmanager.cpp
@@ -1,25 +1,17 @@
 #include "fpsmanager.h"
+#include <iostream>
 
 float FPSManager::m_lastFrame;
 float FPSManager::m_previousFrame;
-unsigned int FPSManager::m_FPS;
+unsigned int FPSManager::m_FPSTime;
 float FPSManager::m_whenGameStarted;
 
-FPSManager::FPSManager()
+void FPSManager::Initialize(unsigned int FPS)
 {
-    m_lastFrame = 0f;
-    m_previousFrame = 0f;
-    m_FPS = 1000;
-    m_whenGameStarted = std::chrono::duration_cast<
-            std::chrono::duration<float, std::milli>>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-}
-
-void FPSManager::updateLastFrameTime()
-{
+    m_FPSTime = 1000.f / FPS;
+    m_lastFrame = getNow();
     m_previousFrame = m_lastFrame;
-    
-    m_lastFrame = std::chrono::duration_cast<
-            std::chrono::duration<float, std::milli>>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+    m_whenGameStarted = m_lastFrame;
 }
 
 float FPSManager::getElapsedTime()
@@ -34,20 +26,33 @@ float FPSManager::getTime()
 
 float FPSManager::makeCalcWithElapsedTime(float someValue)
 {
-    return (this->getElapsedTime() / 1000) * someValue;
-}
-
-void FPSManager::setFPS(unsigned int fps)
-{
-    m_FPS = fps;
+    return (getElapsedTime() / 1000) * someValue;
 }
 
 bool FPSManager::shouldDrawNewFrame()
 {
-    if((1000 / m_FPS) >= getElapsedTime()){
+    if(m_FPSTime < timePassedSinceLastFrame()){
         updateLastFrameTime();
         return true;
-    } else {
-        return false;
     }
+
+    return false;
+}
+
+void FPSManager::updateLastFrameTime()
+{
+    m_previousFrame = m_lastFrame;
+
+    m_lastFrame = getNow();
+}
+
+float FPSManager::timePassedSinceLastFrame()
+{
+    return getNow() - m_lastFrame;
+}
+
+float FPSManager::getNow()
+{
+    return std::chrono::duration_cast<std::chrono::duration<
+            float, std::milli>>(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
